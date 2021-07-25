6c91785bd29425e483a562087659e5940b061275
A.Zakharov <mrmajesticmr90@gmail.com>
2021-07-25 21:59:48 +0300

    Try out repeatOnLifecycle

--- app/src/main/java/com/majestic_dev/coroutinesflow/ui/main/MainScreen.kt
+++ app/src/main/java/com/majestic_dev/coroutinesflow/ui/main/MainScreen.kt
@@ -4,20 +4,18 @@ import android.view.View
 import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.majestic_dev.coroutinesflow.R
 import com.majestic_dev.coroutinesflow.base.ViewBindingSupportFragment
 import com.majestic_dev.coroutinesflow.databinding.SMainBinding
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 class MainScreen : ViewBindingSupportFragment<SMainBinding>(R.layout.s_main) {
 
     private val viewModel: ViewModel by viewModels()
-
-    private var singleEventsJob: Job? = null
 
     override fun bind(view: View): SMainBinding =
         SMainBinding.bind(view)
@@ -31,29 +29,22 @@ class MainScreen : ViewBindingSupportFragment<SMainBinding>(R.layout.s_main) {
             viewModel.message.collect { binding?.tvText?.text = it }
         }
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.eventsFlow.collect {
+                    when (it) {
+                        is Event.ShowToast -> {
+                            Toast.makeText(requireContext(), it.text, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
         binding?.bToggle?.setOnClickListener {
             viewModel.changeMessage("Message from screen")
             viewModel.toggleVisibility()
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        singleEventsJob = viewModel.eventsFlow
-            .onEach {
-                when (it) {
-                    is Event.ShowToast -> {
-                        Toast.makeText(requireContext(), it.text, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }.launchIn(viewLifecycleOwner.lifecycleScope)
-    }
-
-    override fun onStop() {
-        singleEventsJob?.cancel()
-
-        super.onStop()
     }
 
 }
